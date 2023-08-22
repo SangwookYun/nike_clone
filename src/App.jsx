6bae047b22d2bd040dbf5e3b4bab1cde1ff50882
Yun <sangwook.yun@sap.com>
2023-08-22 10:16:32 -0700

    First Update

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import Nav from "./components/Nav";
+import { CustomerReview, 
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  Footer
+} from "./sections";
+
+
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="x1:padding-1 wide:padding-r padding-b">
+      <Hero/>
+    </section>
+    <section className = "padding">
+      <PopularProducts/>
+    </section>
+    <section className = "padding">
+      <SuperQuality/>
+    </section>
+    <section className = "padding">
+      <Services/>
+    </section>
+    <section className = "padding">
+      <SpecialOffer/>
+    </section>
+    <section className = "bg-pale-blue padding">
+      <CustomerReview/>
+    </section>
+    <section className = "padding-x sm:py-32 py-16 w-full">
+      <Subscribe/>
+    </section>
+    <section className = "bg-black padding-x padding-t pb-8">
+      <Footer/>
+    </section>
+  </main>
+)
+
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReview from "./CustomerReview";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+
+export {
+    Hero,
+    CustomerReview,
+    Footer,
+    PopularProducts,
+    SpecialOffer,
+    SuperQuality,
+    Subscribe,
+    Services
+}
